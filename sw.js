b2e4c183ab9496bb1b0e7a56f03b1847622e0e6a
wuwubijiu677-netizen <wuwubijiu677@gmail.com>
2025-12-23 19:09:15 +0900

    Refactor service worker for simplified caching

--- sw.js
+++ sw.js
@@ -1,22 +1,4 @@
-const CACHE_NAME = 'pwa-logbook-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', e => {
+  self.skipWaiting();
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener('fetch', e => {});
